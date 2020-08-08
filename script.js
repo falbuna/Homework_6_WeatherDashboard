53035ddd4f66a303f33537c8e4f72df1ee0dba25
Francis Albuna <falbuna1@gmail.com>
2020-08-07 20:04:23 -0700

    Got the buttons to create and append when a new search is done. Got the date to display next to the city using moment.js.

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
+const Cities = [];
+
 initial();
 
 function initial(){
@@ -16,7 +18,8 @@ const WeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City +
     method: "GET"
     }).then(function(response){
         console.log(response);
-        $('.city').html('<h3>' + response.name + ' (Date) </h3>');
+        const TodaysDate = moment().format('MM/DD/YYYY');
+        $('.city').html('<h3>' + response.name + ' (' + TodaysDate + ')' + '</h3>');
         const tempF = (response.main.temp - 273.15) * 1.80 + 32;
         $('.temp').text('Temperature: ' + tempF.toFixed(2) + ' F');
         $('.humidity').text('Humidity: ' + response.main.humidity + '%');
@@ -26,10 +29,16 @@ const WeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City +
 
 $('.btn').on('click', function(event){
     event.preventDefault();
-    // console.log("I've been clicked!");
     const cityinput = $('#city-input').val().trim();
-    // console.log(cityinput)
     localStorage.setItem('city', cityinput);
 
+    Cities.push(cityinput);
+
     displayweatherinfo();
-    })
+
+    const cityBtnDiv = $('<button>');
+    cityBtnDiv.text(cityinput);
+    cityBtnDiv.addClass("city-btn");
+    $("#city-buttons").prepend(cityBtnDiv);
+    
+    });
