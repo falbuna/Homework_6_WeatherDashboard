9ba04670a258e741a5d7fd78b2dbb9f1e8860941
Francis Albuna <falbuna1@gmail.com>
2020-08-10 21:18:59 -0700

    Got the 5-Day Forcast to clear the previous city and only display the current city. Added comments to script.js file.

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // This will hold the Cities previously searched.
 const Cities = [];
-// TodaysDate is gotten from moment.js and puts it in MM/DD/YYYY Format
+// TodaysDate is gotten from moment.js and puts it in MM/DD/YYYY Format.
 const TodaysDate = moment().format('MM/DD/YYYY');
 
 // This calls the initialize function
@@ -16,16 +16,16 @@ function displayweatherinfo(){
 
 // The City constant gets pulled from local storage
 const City = localStorage.getItem('city')
-    // This is the URL for the Open Weather Map API that will change depending on the city input
+    // This is the URL for the Open Weather Map API that will change depending on the city input.
     const WeatherURL = "https://api.openweathermap.org/data/2.5/weather?q=" + City + ",US&appid=3cc36befffcdde3fee1a588394a435ef";
     // The Ajax for the Open Weather API
     $.ajax({
     url: WeatherURL,
     method: "GET"
     }).then(function(response){
-        // This constant will change the temperature from Kelvin to Fahrenheit
+        // This constant will change the temperature from Kelvin to Fahrenheit.
         const tempF = (response.main.temp - 273.15) * 1.80 + 32;
-        // This constant will get the value of the weather icon
+        // This constant will get the value of the weather icon.
         const Weathericon = response.weather[0].icon;
 
         // This will get the name of the city from the Ajax response and the icon from the open weather map website. The date is retrieved from moment.js from the beginning of this doc.
@@ -37,7 +37,7 @@ const City = localStorage.getItem('city')
         // The windspeed is pulled from the Ajax resposne and inserted to the html.
         $('.windspd').text('Wind Speed: ' + response.wind.speed + ' MPH');
         
-        // The latitude and longitute coordinates are needed for the UV and 5 Day Forcast Ajax requests
+        // The latitude and longitute coordinates are needed for the UV and 5 Day Forcast Ajax requests.
         const lati = response.coord.lat;
         const longi = response.coord.lon;
             
@@ -63,62 +63,73 @@ const City = localStorage.getItem('city')
                     }
             });
 
+            // Thjis is the URL for the 5-day Forecast Request from the Open Weather Map. The latitude and longitude are needed.
             const forcastURL = "https://api.openweathermap.org/data/2.5/onecall?lat=" + lati + "&lon=" + longi + "&exclude=current,minutely,hourly,&appid=3cc36befffcdde3fee1a588394a435ef";
+            
+            // This will display the 5-Day Forecast Function
+            renderfivedayforecast();
 
+            // This is the function for the 5-Day Forecast
+            function renderfivedayforecast() {
+                // This will empty the 5-Day Forecast for the previous city so that only the current city will show.
+                $("#forcastRow").empty();
+            // This is the Ajax request for the 5-day Forecast.
             $.ajax({
                 url: forcastURL,
                 method: "GET"
             }).then(function(response){
-
+                // This is the Row for the 5-Day Forcast that will display as cards.
                 const cards = $("#forcastRow");
-
+                // This is the for loop to loop through the array that is generated from the Ajax request response.
                 for (let i = 1; i <= 5; i++){
+                // The will get the time in UTC format from the Ajax Reponse.
                 let UTCFormat = response.daily[i].dt;
+                // This will use moment.js to convert the time from UTC to Standard Date Format.
                 const ForDate = moment.unix(UTCFormat).format("MM/DD/YYYY");
-
+                // This will store the weather icon from the Ajax response.
                 const ForWIcon = response.daily[i].weather[0].icon;              
-                
+                // This will convert the temperature from Kelvin to Fahrenheit.
                 const FortempF = (((response.daily[i].feels_like.day - 273.15) * 1.80 + 32).toFixed(2));
-
+                // This will get the humidity from teh Ajax response.
                 const Forhumidity = (response.daily[i].humidity);
-
+                // This will create a div for the cards for the 5-day Forecast.
                 const cardDiv = $("<div>");
                 cardDiv.addClass("card bg-primary");
                 cardDiv.attr("id", "cards")
-
+                // This will create a div for the Date.
                 const dateDiv = $("<div>");
                 dateDiv.attr('id', 'Date: ' + ForDate);
                 dateDiv.html('<h5>' + ForDate + '</h5>');
                 dateDiv.addClass('cardDate');
-
+                // This will append the date to the card Div.
                 cardDiv.append(dateDiv);
-
+                // This will create a div for the weather icon.
                 const iconDiv = $("<div>");
                 iconDiv.attr('id', 'wIcon');
                 iconDiv.html('<img id="wIcon" src="https://openweathermap.org/img/wn/' + ForWIcon + '@2x.png" />');
                 iconDiv.addClass('cardIcon');
-
+                // This will append the icon to the card Div.
                 cardDiv.append(iconDiv);
-
+                // This will create a div for the temperature.
                 const tempDiv = $("<div>");
                 tempDiv.attr('id', 'Temp:' + FortempF);
                 tempDiv.html('Temp: ' + FortempF + ' &#8457')
                 tempDiv.addClass('cardTemp');
-
+                // This will append the temperature to the card Div.
                 cardDiv.append(tempDiv);
-
+                // This will create a div for the humidity.
                 const humidityDiv = $("<div>");
                 humidityDiv.attr('id', 'Humidity:' + Forhumidity);
                 humidityDiv.html('Humidity: ' + Forhumidity + '%');
                 humidityDiv.addClass('cardHumidity');
-
+                // This will append the humidity to the card Div.
                 cardDiv.append(humidityDiv);
-
+                // The cards will be appeneded to the 5-Day Forcast Row.
                 cards.append(cardDiv);
-
                 };
-            });
+            });};
         });
+        
 };
 
 // This will signify that if the search button is clicked, the searched city will be displayed and saved to local storage.
